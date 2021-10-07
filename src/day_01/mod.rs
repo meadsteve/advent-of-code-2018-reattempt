f0c7e93068203cded314b37d1d9f7c5e87b7d041
steve <steve@meadsteve.dev>
2021-10-07 16:04:04 +0200

    part one solution

--- src/day_01/mod.rs
+++ src/day_01/mod.rs
@@ -1,13 +1,43 @@
+use std::fs;
 use crate::AdventDay;
 
 pub struct DayOne();
 
 impl AdventDay for DayOne {
     fn run_part_one(&self) {
-        todo!()
+        let contents = fs::read_to_string("./data/day01.txt")
+            .expect("The input file was missing");
+        let frequencies = contents.split("\n")
+            .filter(|&line| line != "")
+            .collect();
+        println!("Answer: {}", sum_frequencies(frequencies));
     }
 
     fn run_part_two(&self) {
         todo!()
+    }
+}
+
+fn sum_frequencies(frequencies: Vec<&str>) -> i64 {
+    frequencies.iter().map(convert_to_int).sum()
+}
+
+fn convert_to_int(freq: &&str) -> i64 {
+    if freq.starts_with("+") {
+        freq[1..].parse::<i64>().unwrap()
+    } else {
+        freq[1..].parse::<i64>().unwrap() * -1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day_01::sum_frequencies;
+
+    #[test]
+    fn it_works_for_a_list_of_frequency_strings() {
+        let input = vec!["+1", "+3", "-2"];
+        let sum = sum_frequencies(input);
+        assert_eq!(sum, 2);
     }
 }
