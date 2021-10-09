49a6568f84e8c134b4175baf9f5257052ab8d48a
steve <steve@meadsteve.dev>
2021-10-09 12:15:25 +0200

    tidy up for day 4

---
+++ src/day_04/mod.rs
@@ -0,0 +1,14 @@
+use crate::helpers::DayData;
+use crate::AdventDay;
+
+pub struct DayFour();
+
+impl AdventDay for DayFour {
+    fn run_part_one(&self) -> String {
+        todo!()
+    }
+
+    fn run_part_two(&self) -> String {
+        todo!()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use adventofcode2018_reattempt::day_01::DayOne;
 use adventofcode2018_reattempt::day_02::DayTwo;
 use adventofcode2018_reattempt::day_03::DayThree;
+use adventofcode2018_reattempt::day_04::DayFour;
 use adventofcode2018_reattempt::AdventDay;
 use std::env;
 
@@ -10,12 +11,14 @@ fn main() {
     let day = &args[1];
     let part = &args[2];
 
-    let result = match &day[..] {
-        "01" => run_day(&DayOne(), part),
-        "02" => run_day(&DayTwo(), part),
-        "03" => run_day(&DayThree(), part),
+    let solution: &(dyn AdventDay) = match &day[..] {
+        "01" => &DayOne(),
+        "02" => &DayTwo(),
+        "03" => &DayThree(),
+        "04" => &DayFour(),
         _ => panic!("That day has not been done"),
     };
+    let result = run_day(solution, part);
     println!("{}", result);
 }
 
