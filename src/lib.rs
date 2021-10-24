32acb72c8bf783b6ae7f51e7757213755e9b2ee0
steve <steve@meadsteve.dev>
2021-10-24 09:39:15 +0200

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,48 @@ pub mod day_04;
 
 mod helpers;
 
+use day_01::DayOne;
+use day_02::DayTwo;
+use day_03::DayThree;
+use day_04::DayFour;
+
+use structopt::StructOpt;
+
 pub trait AdventDay {
     fn run_part_one(&self) -> String;
     fn run_part_two(&self) -> String;
+}
+
+#[derive(StructOpt)]
+pub struct CliArgs {
+    #[structopt(short, long)]
+    day: String,
+    #[structopt(short, long)]
+    part: String,
+}
+
+pub fn run(
+    args: &CliArgs,
+    mut writer: impl std::io::Write,
+) -> Result<(), Box<dyn std::error::Error>> {
+    writeln!(writer, "Hello, advent of code")?;
+
+    let solution: &(dyn AdventDay) = match &args.day[..] {
+        "01" => &DayOne(),
+        "02" => &DayTwo(),
+        "03" => &DayThree(),
+        "04" => &DayFour(),
+        _ => panic!("That day has not been done"),
+    };
+    let result = run_day(solution, &args.part[..]);
+    writeln!(writer, "{}", result)?;
+    Ok(())
+}
+
+fn run_day(solution: &dyn AdventDay, part: &str) -> String {
+    match part {
+        "01" => solution.run_part_one(),
+        "02" => solution.run_part_two(),
+        _ => panic!("Day must be 01 or 02"),
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,7 @@
-use adventofcode2018_reattempt::day_01::DayOne;
-use adventofcode2018_reattempt::day_02::DayTwo;
-use adventofcode2018_reattempt::day_03::DayThree;
-use adventofcode2018_reattempt::day_04::DayFour;
-use adventofcode2018_reattempt::AdventDay;
+use adventofcode2018_reattempt::{run, CliArgs};
 use structopt::StructOpt;
 
-#[derive(StructOpt)]
-struct CliArgs {
-    #[structopt(short, long)]
-    day: String,
-    #[structopt(short, long)]
-    part: String,
-}
-
-fn main() {
-    println!("Hello, advent of code");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: CliArgs = CliArgs::from_args();
-
-    let solution: &(dyn AdventDay) = match &args.day[..] {
-        "01" => &DayOne(),
-        "02" => &DayTwo(),
-        "03" => &DayThree(),
-        "04" => &DayFour(),
-        _ => panic!("That day has not been done"),
-    };
-    let result = run_day(solution, &args.part[..]);
-    println!("{}", result);
-}
-
-fn run_day(solution: &dyn AdventDay, part: &str) -> String {
-    match part {
-        "01" => solution.run_part_one(),
-        "02" => solution.run_part_two(),
-        _ => panic!("Day must be 01 or 02"),
-    }
+    run(&args, &mut std::io::stdout())
 }
