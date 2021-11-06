c0e1054635cf08012d7672ccf20823bfbe4b059f
steve <steve@meadsteve.dev>
2021-11-06 09:56:57 +0100

    tidy up

--- src/day_05/mod.rs
+++ src/day_05/mod.rs
@@ -1,5 +1,3 @@
-mod linked_letters;
-
 use crate::helpers::DayData;
 use crate::AdventDay;
 use std::sync::{Arc, Mutex};
@@ -9,7 +7,7 @@ pub struct DayFive();
 impl AdventDay for DayFive {
     fn run_part_one(&self) -> String {
         let input = DayData::from_file_path("./data/day05.txt").first_line();
-        format!("the result: {}", case_replace_repeat(input).len())
+        format!("the result: {}", replacer::remove_case_pairs(input).len())
     }
 
     fn run_part_two(&self) -> String {
@@ -22,65 +20,72 @@ impl AdventDay for DayFive {
             input_without_c = input_without_c.replace(c, "");
             input_without_c = input_without_c.replace(c.to_uppercase().next().unwrap(), "");
             pool.execute(move || {
-                let result = case_replace_repeat(input_without_c).len();
+                let result = replacer::remove_case_pairs(input_without_c).len();
                 local_results.lock().unwrap().push((c, result));
             });
         }
         pool.join();
-        results.lock().unwrap().sort_by(|(_, a), (_, b)| a.cmp(b));
-        let answer = *results.lock().unwrap().first().unwrap();
+        let mut results = results
+            .lock()
+            .expect("Problem occured getting the results from the threads");
+        results.sort_by(|(_, a), (_, b)| a.cmp(b));
+        let answer = *results
+            .first()
+            .expect("The results list was strangely empty");
         format!("Letter {} with {}", answer.0, answer.1)
     }
 }
 
-enum ReplacementResult {
-    ReplacementsMade(Vec<char>),
-    Unchanged(Vec<char>),
-}
-
-fn case_replace(mut input: Vec<char>) -> ReplacementResult {
-    let pairs_to_remove = find_pair_to_remove(&input);
-    match pairs_to_remove.len() {
-        0 => ReplacementResult::Unchanged(input),
-        _ => {
-            for (x, y) in pairs_to_remove {
-                input[x] = ' ';
-                input[y] = ' ';
-            }
-            ReplacementResult::ReplacementsMade(input)
-        }
+mod replacer {
+    enum ReplacementResult {
+        ReplacementsMade(Vec<char>),
+        Unchanged(Vec<char>),
     }
-}
 
-fn find_pair_to_remove(input: &[char]) -> Vec<(usize, usize)> {
-    let mut output = Vec::new();
-    let mut letters = input
-        .iter()
-        .enumerate()
-        .filter(|(_, &c)| c != ' ')
-        .peekable();
-    while let Some((pos_first, first)) = letters.next() {
-        let next = letters.peek();
-        if let Some((pos_second, second)) = next {
-            if first.to_uppercase().next() == second.to_uppercase().next() && &first != second {
-                output.push((pos_first, *pos_second));
-                letters.next();
+    pub fn remove_case_pairs(input: String) -> String {
+        let mut chars: Vec<char> = input.chars().collect();
+        loop {
+            let result = single_pass_replace(chars);
+            match result {
+                ReplacementResult::Unchanged(answer) => {
+                    return answer.into_iter().filter(|&c| c != ' ').collect()
+                }
+                ReplacementResult::ReplacementsMade(next) => chars = next,
             }
         }
     }
-    output
-}
 
-fn case_replace_repeat(input: String) -> String {
-    let mut chars: Vec<char> = input.chars().collect();
-    loop {
-        let result = case_replace(chars);
-        match result {
-            ReplacementResult::Unchanged(answer) => {
-                return answer.into_iter().filter(|&c| c != ' ').collect()
+    fn single_pass_replace(mut input: Vec<char>) -> ReplacementResult {
+        let pairs_to_remove = find_pair_to_remove(&input);
+        match pairs_to_remove.len() {
+            0 => ReplacementResult::Unchanged(input),
+            _ => {
+                for (x, y) in pairs_to_remove {
+                    input[x] = ' ';
+                    input[y] = ' ';
+                }
+                ReplacementResult::ReplacementsMade(input)
             }
-            ReplacementResult::ReplacementsMade(next) => chars = next,
         }
+    }
+
+    fn find_pair_to_remove(input: &[char]) -> Vec<(usize, usize)> {
+        let mut output = Vec::new();
+        let mut letters = input
+            .iter()
+            .enumerate()
+            .filter(|(_, &c)| c != ' ')
+            .peekable();
+        while let Some((pos_first, first)) = letters.next() {
+            let next = letters.peek();
+            if let Some((pos_second, second)) = next {
+                if first.to_uppercase().next() == second.to_uppercase().next() && &first != second {
+                    output.push((pos_first, *pos_second));
+                    letters.next();
+                }
+            }
+        }
+        output
     }
 }
 
@@ -92,7 +97,7 @@ mod tests {
     fn test_replaces_differently_cased_pairs_repeatedly() {
         assert_eq!(
             "dabCBAcaDA".to_string(),
-            case_replace_repeat("dabAcCaCBAcCcaDA".to_string())
+            replacer::remove_case_pairs("dabAcCaCBAcCcaDA".to_string())
         )
     }
 }
